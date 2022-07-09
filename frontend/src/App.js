003144714fae04a43ba1e01ccac4c7517d74827a
rozh <r.bahman98@gmail.com>
2022-07-09 19:04:56 +0300

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">capstone</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
